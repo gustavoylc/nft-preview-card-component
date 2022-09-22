a522756e9b01424efbd6dfce2d8051458d7b77c6
Gustavo Leyte-Vidal Chacón <glcdmx@gmail.com>
2022-09-22 10:06:02 -0400

    fix: center card

--- src/components/CardContentCreator/index.jsx
+++ src/components/CardContentCreator/index.jsx
@@ -1,20 +1,16 @@
 import React from "react";
-import { CardMedia } from "@components/CardMedia";
 import avatar from "@images/image-avatar.png";
 
 export const CardContentCreator = () => {
 	return (
-		<div className="card-content-creator-container">
-			<CardMedia
-				image={avatar}
+		<div className="card-content-creator-container">			
+			<img
+				className="card-content-creator-container__avatar"
+				src={avatar}
 				alt="avatar"
-				height="33px"
-				width="40px"
-				border="1px solid white"
-				borderRadiusRounded={true}
 			/>
 			<p className="card-content-creator-container__owner">
-				Creation of{" "}
+				Creation of
 				<a
 					className="card-content-creator-container__owner__name"
 					href="#"
--- src/components/CardMedia/index.jsx
+++ src/components/CardMedia/index.jsx
@@ -5,23 +5,16 @@ export const CardMedia = ({
 	alt,
 	height,
 	width,
-	border,
-	borderRadiusRounded,
 	viewImage,
 }) => {
 	return (
 		<figure className="card-media-container">
 			<img
-				className={` ${
-					borderRadiusRounded
-						? "card-media-container__image__avatar"
-						: "card-media-container__image"
-				}`}
+				className="card-media-container__image"				
 				src={image}
 				alt={alt}
 				height={height}
 				width={width}
-				border={border}
 			/>			
 			<figure className="card-media-container__image-eye">
 				<img
